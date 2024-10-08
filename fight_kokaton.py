275213f7d9e4950a017e381de76556a9474cc74b
takeru <c0b23085f4@edu.teu.ac.jp>
2024-10-08 14:46:05 +0900

    happyの実装

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -170,6 +170,8 @@ def main():
             if bomb is not None:
                 if beam.rct.colliderect(bomb.rct):  # ビームと爆弾が衝突したら
                     beam, bomb = None, None
+                    bird.change_img(6, screen)
+                    pg.display.update() 
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
